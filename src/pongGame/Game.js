b156eb9971f22998713077267ae6b053e0013db2
Claire Lu <appsfunlu@gmail.com>
2021-08-27 09:49:34 -0700

    added two circles to test it out

--- src/pongGame/Game.js
+++ src/pongGame/Game.js
@@ -5,14 +5,26 @@ const Game = props => {
   //To get Canvas DOM element, use useRef
   const canvasRef = useRef(null)
 
+  const draw = ctx => {\
+    // added two filled circles.
+    ctx.fillStyle = '#ffffff'
+    ctx.beginPath()
+    ctx.arc(50, 100, 5, 0, 2*Math.PI)
+    ctx.fill()
+
+    ctx.fillStyle = '#ffffff'
+    ctx.beginPath()
+    ctx.arc(100, 100, 5, 0, 2*Math.PI)
+    ctx.fill()
+  }
+
   //this is to  get the context, useEffect let us call the functions right after  the component did mount
   useEffect(() => {
     const canvas = canvasRef.current
     const context = canvas.getContext('2d')
     //Our first draw
-
-  }, [])
-  
+    draw(context)
+  }, [draw])
 
   return (
     <div>
