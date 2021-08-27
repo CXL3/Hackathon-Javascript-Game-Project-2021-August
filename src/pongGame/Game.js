fbd98e56a283b3484664ff6b738f9c2221c29f8f
Claire Lu <appsfunlu@gmail.com>
2021-08-27 10:09:12 -0700

    added two paddles, one for the player, one for the computer, You can tell, we are building a pong game.

--- src/pongGame/Game.js
+++ src/pongGame/Game.js
@@ -5,17 +5,23 @@ const Game = props => {
   //To get Canvas DOM element, use useRef
   const canvasRef = useRef(null)
 
-  const draw = ctx => {\
-    // added two filled circles.
+  const draw = ctx => {
+    // added a ball
     ctx.fillStyle = '#ffffff'
     ctx.beginPath()
     ctx.arc(50, 100, 5, 0, 2*Math.PI)
     ctx.fill()
-
+    
+    //Player Paddle 
     ctx.fillStyle = '#ffffff'
-    ctx.beginPath()
-    ctx.arc(100, 100, 5, 0, 2*Math.PI)
+    ctx.fillRect(100, 670, 50, 6)
     ctx.fill()
+
+    //Computer Paddle 
+    ctx.fillStyle = '#ffffff'
+    ctx.fillRect(100, 30, 50, 6)
+    ctx.fill()
+
   }
 
   //this is to  get the context, useEffect let us call the functions right after  the component did mount
